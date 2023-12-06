473c4c53bb6fc85448f4bc20ba77ee7b6b913c9e
ankitmtiwari <tankit291389@gmail.com>
2023-12-06 22:06:36 +0530

    dashbord started

---
+++ src/app/components/main_component/Dashboard.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="h-[100vh]  m-5">
+        <div className="bg-white flex justify-between border-2">
+          <div className="w-[35%] border-r-2">
+            <div className="bg-purple-800 h-[25vh] p-16 text-2xl text-white font-semibold rounded-br-full">
+              Nav bar
+            </div>
+            <div className="all_profiles h-[75vh] bg-white  p-6  overflow-y-auto">
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[65%]">side bar full screen</div>
+        </div>
+      </div>
+
+      {/* <div className="w-[100%] h-[100vh] relative">
+        <div className=" bg-white flex justify-between border-2">
+          <div className="w-[35%] border-r-2 ">
+            <div className="bg-purple-800 p-16 text-2xl text-white font-semibold rounded-br-full">
+              Nav bar
+            </div>
+            <div className="all_profiles  bg-white px-4 py-6">
+              <div className="border-2  flex rounded-xl shadow-md m-3 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="border-2  flex rounded-xl shadow-md m-2 p-2">
+                <div className="h-16 w-16  rounded-full bg-slate-800 overflow-hidden">
+                  <img
+                    src={"/profile.jpg"}
+                    className="w-[100%] h-[100%] object-cover"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[65%]">side bar full screen</div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default Dashboard;
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Hisab_card from "./components/single_components/hisab_card";
 import Auth from "./components/main_component/auth";
+import Dashboard from "./components/main_component/Dashboard";
 const Page = () => {
   return (
     <>
-      <Auth />
+      <Dashboard />
+      {/* <Auth />
       <div id="all_hisab_cards">
         <Hisab_card amt="1025600" img="/profile.jpg" />
         <Hisab_card amt="-150" img="/profile.jpg" />
         <Hisab_card amt="100" img="/profile.jpg" />
         <Hisab_card amt="-100" img="/profile.jpg" />
-      </div>
-
+      </div> */}
     </>
   );
 };
